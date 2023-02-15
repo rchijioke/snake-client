1a3ea583e49686dd86eecba422681983137d36c7
Roy <ekeneroy@gmail.com>
2023-02-15 16:37:31 -0600

    client2

---
+++ client2.js
@@ -0,0 +1,24 @@
+
+
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+
+    host: "165.227.47.243", // change to IP address of computer, more on that below
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+conn.on("connect", () => {
+  console.log("connection successfull")
+});
+  return conn;
+};
+console.log("Connecting ...");
+const connection = connect();
+
+module.exports = {
+  net,
+  connection
+}
