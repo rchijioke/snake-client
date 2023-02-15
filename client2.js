d22250aeeb81ab5f60b635b2d4dbd046a20a7318
Roy <ekeneroy@gmail.com>
2023-02-15 16:44:01 -0600

    client2 updated

--- client2.js
+++ client2.js
@@ -11,7 +11,7 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 conn.on("connect", () => {
-  console.log("connection successfull")
+  console.log("Successfully connected to game server", "Name: wat" )
 });
   return conn;
 };
